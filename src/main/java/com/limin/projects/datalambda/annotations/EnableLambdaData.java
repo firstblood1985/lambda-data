e1475ec15006fec5196f7ec24a4a047468ec3a1b
firstblood1985 <limin.hust@foxmail.com>
2022-04-16 19:47:25 +0800

    fix init issue

--- src/main/java/com/limin/projects/datalambda/annotations/EnableLambdaData.java
+++ src/main/java/com/limin/projects/datalambda/annotations/EnableLambdaData.java
@@ -1,6 +1,7 @@
 package com.limin.projects.datalambda.annotations;
 
 import com.limin.projects.datalambda.config.LambdaInit;
+import com.limin.projects.datalambda.config.LambdaRawConfig;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
@@ -15,6 +16,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import(LambdaInit.class)
+@Import({LambdaInit.class, LambdaRawConfig.class})
 public @interface EnableLambdaData {
 }
